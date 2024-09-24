ae239daabe801e6ffd7dfb289470c73501897922
Simeon199 <sim.ki@outlook.de>
2024-09-24 12:54:04 +0200

    many changes and new files (style nearly completed)

---
+++ js/start.js
@@ -0,0 +1,11 @@
+function redirectToPlayPage() {
+    window.location.href = "index.html";
+}
+
+function redirectToLegalNoticePage() {
+    window.location.href = "legal_notice.html"
+}
+
+function redirectToStart() {
+    window.location.href = "start.html";
+}
