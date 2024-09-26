2c357a065dc06c643e1f6fd6bf9cee500be468c6
Simeon199 <sim.ki@outlook.de>
2024-09-26 13:02:04 +0200

    first steps in making index.html responsive

--- js/game.js
+++ js/game.js
@@ -50,6 +50,22 @@ function preventBubbling(event) {
     event.stopPropagation();
 }
 
+// Open and Close Mini-Version of All-Icons-Container (Kann man in einer Funktion behandeln --> später: Vereinfache dies!)
+
+function openAllIconsContainer() {
+    let allIconsMiniVersionContainer = document.getElementById('all-icons-container-mini-version');
+    if (allIconsMiniVersionContainer.style.display !== 'flex') {
+        allIconsMiniVersionContainer.style.display = ' flex';
+    }
+}
+
+function closeAllIconsContainer() {
+    let allIconsMiniVersionContainer = document.getElementById('all-icons-container-mini-version');
+    if (allIconsMiniVersionContainer.style.display !== 'none') {
+        allIconsMiniVersionContainer.style.display = ' none';
+    }
+}
+
 // Start-or-Stop Game Related Logic
 
 function startGame() {
@@ -80,7 +96,6 @@ function clearAllIntervals() {
 }
 
 function playAgain() {
-    document.getElementById('overlay').focus();
     document.getElementById('canvas-container').style.display = 'block';
     document.getElementById('canvas').style.display = 'block';
     document.getElementById('losing-image').style.display = 'none';
