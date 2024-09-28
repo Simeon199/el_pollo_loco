96de3d2139aa97f73f7c7e525d95680eeb67b49c
Simeon199 <sim.ki@outlook.de>
2024-09-28 22:41:32 +0200

    responsive styling nearly finished in start.html

--- js/start.js
+++ js/start.js
@@ -9,3 +9,19 @@ function redirectToLegalNoticePage() {
 function redirectToStart() {
     window.location.href = "start.html";
 }
+
+// Open and Close Mini-Version of All-Icons-Container (Kann man in einer Funktion behandeln --> später: Vereinfache dies!)
+
+function openAllIconsContainer() {
+    let allIconsMiniVersionContainer = document.getElementById('all-icons-container-mini-version');
+    if (allIconsMiniVersionContainer.style.display !== 'flex') {
+        allIconsMiniVersionContainer.style.display = ' flex';
+    }
+}
+
+function closeAllIconsContainer() {
+    let allIconsMiniVersionContainer = document.getElementById('all-icons-container-mini-version');
+    if (allIconsMiniVersionContainer.style.display !== 'none') {
+        allIconsMiniVersionContainer.style.display = ' none';
+    }
+}
