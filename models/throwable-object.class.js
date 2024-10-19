09261a35fcf493692d1d308ea746cde94017c3ce
Simeon199 <sim.ki@outlook.de>
2024-10-19 15:36:08 +0200

    throwable-object.class.js finished with shortening functions

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -41,8 +41,6 @@ class ThrowableObject extends MovableObject {
         this.keyboard = z;
     }
 
-    // Aktuell ist hier keine animate Funktion vorhanden --> Orientierung an der Implementierung in Character!
-
     checkIfBottleIsAlreadyBroken() {
         if (this.isBottleBroken == true) {
             this.img.src = '';
@@ -59,69 +57,70 @@ class ThrowableObject extends MovableObject {
         } else if (this.pepeIsWatchingLeft()) {
             this.applyThrowingSetInterval('left');
         }
+        this.handleSpeedAndGravityOfThrowedBottle();
+    }
+
+    handleSpeedAndGravityOfThrowedBottle() {
         this.speedY = 30;
         this.applyGravity();
     }
 
     applyThrowingSetInterval(direction) {
-        if (direction == 'left') {
-            this.throwBottleToTheLeft();
-        } else if (direction == 'right') {
-            this.throwBottleToTheRight();
-        }
+        this.throwBottleFunction(direction);
     }
 
-
-    // --- To-Do: Bündle throwBottleToTheRight() und ThrowBottleToTheLeft() in einer Funktion ---
-
-    throwBottleToTheRight() {
+    clearIntervalIDFunction() {
         if (this.intervalID) {
             clearInterval(this.intervalID);
         }
-        if (this.isBottleBroken == false) {
-            this.intervalID = setInterval(() => {
-                if (this.y >= 350) {
-                    this.stopGame('right');
-                } else {
-                    this.x += 10;
-                    this.playAnimation(this.BOTTLE_ROTATE_IMAGES);
-                }
-            }, 25);
-        } else if (this.isBottleBroken == true) {
-            this.playBottleBrokenAnimation();
+    }
+
+    checkDirectionLeftOrRightMovingBottle(direction) {
+        if (direction == 'right') {
+            this.x += 10;
+        } else if (direction == 'left') {
+            this.x -= 10;
         }
     }
 
-    throwBottleToTheLeft() {
-        if (this.intervalID) {
-            clearInterval(this.intervalID);
+    checkWhetherBottleIsFlyingInTheAirOrLanding(direction) {
+        if (this.y >= 350) {
+            this.stopGame(direction);
+        } else {
+            this.checkDirectionLeftOrRightMovingBottle(direction);
+            this.playAnimation(this.BOTTLE_ROTATE_IMAGES);
         }
+    }
+
+    throwBottleFunction(direction) {
+        this.clearIntervalIDFunction();
         if (!this.isBottleBroken) {
             this.intervalID = setInterval(() => {
-                if (this.y >= 350) {
-                    this.stopGame('left');
-                } else {
-                    this.x -= 10;
-                    this.playAnimation(this.BOTTLE_ROTATE_IMAGES);
-                }
+                this.checkWhetherBottleIsFlyingInTheAirOrLanding(direction)
             }, 25);
-        } else if (this.isBottleBroken == true) {
+        } else if (this.isBottleBroken) {
             this.playBottleBrokenAnimation();
         }
     }
 
-    // --- Todo-Ende --- //
+    stopSpeedAccelerationAndHeightOfBottle() {
+        this.speedY = 0;
+        this.acceleration = 0;
+        this.y = 360;
+    }
+
+    setLandingBottleDirection(direction) {
+        if (direction == 'left') {
+            this.img.src = this.STANDING_BOTTLE_RIGHT_DIRECTION[0];
+        } else if (direction == 'right') {
+            this.img.src = this.STANDING_BOTTLE_LEFT_DIRECTION[0];
+        }
+    }
 
     stopGame(direction) {
         if (!this.isBottleBroken) {
-            this.speedY = 0;
-            this.acceleration = 0;
-            this.y = 360;
-            if (direction == 'left') {
-                this.img.src = this.STANDING_BOTTLE_RIGHT_DIRECTION[0];
-            } else if (direction == 'right') {
-                this.img.src = this.STANDING_BOTTLE_LEFT_DIRECTION[0];
-            }
+            this.stopSpeedAccelerationAndHeightOfBottle();
+            this.setLandingBottleDirection(direction);
             this.bottleLanding.play();
             clearInterval(this.intervalID);
         }
@@ -137,23 +136,26 @@ class ThrowableObject extends MovableObject {
         return boleanValue;
     }
 
+    setAndPlayBottleBrokenIntervalId(totalFrames, animationInterval) {
+        this.bottleBrokenIntervalId = setInterval(() => {
+            this.img.src = this.BOTTLE_BROKEN_IMAGES[this.currentImage];
+            this.currentImage++;
+            if (this.currentImage >= totalFrames) {
+                clearInterval(this.bottleBrokenIntervalId);
+                this.img.src = '';
+            }
+        }, animationInterval);
+    }
+
     playBottleBrokenAnimation() {
         this.currentImage = 0;
         let animationInterval = 50;
         let totalFrames = this.BOTTLE_BROKEN_IMAGES.length;
-
         if (this.bottleBrokenIntervalId) {
             clearInterval(this.bottleBrokenIntervalId);
         }
-        if (this.isBottleBroken == true) {
-            this.bottleBrokenIntervalId = setInterval(() => {
-                this.img.src = this.BOTTLE_BROKEN_IMAGES[this.currentImage];
-                this.currentImage++;
-                if (this.currentImage >= totalFrames) {
-                    clearInterval(this.bottleBrokenIntervalId);
-                    this.img.src = '';
-                }
-            }, animationInterval);
+        if (this.isBottleBroken) {
+            this.setAndPlayBottleBrokenIntervalId(totalFrames, animationInterval);
         }
     }
 }
