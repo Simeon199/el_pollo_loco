269e6e41483a26c9df369d6a37efc47bde2475dd
Simeon199 <sim.ki@outlook.de>
2025-04-11 09:06:03 +0200

    proper navigation is now working

---
+++ shared/shared.js
@@ -0,0 +1,11 @@
+function toPrivacyPolicyPage(){
+    window.location.href = "../privacy_policy/privacy_policy.html";
+}
+
+function toLegalNoticePage(){
+    window.location.href = "../legal_notice/legal_notice.html";
+}
+
+function backToGamePage(){
+    window.location.href = "../index.html";
+}
