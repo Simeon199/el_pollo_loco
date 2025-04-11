c8482314261a5eb5dffc26cf9a8f236c68d714e1
Simeon199 <sim.ki@outlook.de>
2025-04-11 12:21:02 +0200

    further improvements on pop up and styling in index.html

--- js/start.js
+++ js/start.js
@@ -24,58 +24,17 @@ function redirectToPrivacyPolicyPage(){
     window.location.href = "./privacy_policy/privacy_policy.html";
 }
 
-/**
- * Opens the container displaying all icons in a minimized version.
- */
-
-function openAllIconsContainer() {
-    let allIconsOverlay = document.getElementById('all-icons-container-overlay');
-    if (allIconsOverlay.classList.contains('d-none')) {
-        allIconsOverlay.classList.remove('d-none');
+function closeOverlay(overlayId){
+    let overlay = document.getElementById(`${overlayId}`);
+    if (!overlay.classList.contains('d-none')) {
+        overlay.classList.add('d-none');
     }
 }
 
-/**
- * Opens the container displaying the imprint and privacy-policy-page.
- */
-
-function openImprintAndPrivacyOverlay(){
-    let imprintAndPrivacyOverlay = document.getElementById('imprint-and-privacy-policy-overlay');
-    if(imprintAndPrivacyOverlay.classList.contains('d-none')){
-        imprintAndPrivacyOverlay.classList.remove('d-none');
-    }
-}
-
-/**
- * Closes the container displaying all icons in a minimized version.
- */
-
-function closeAllIconsContainer() {
-    let allIconsOverlay = document.getElementById('all-icons-container-overlay');
-    if (!allIconsOverlay.classList.contains('d-none')) {
-        allIconsOverlay.classList.add('d-none');
-    }
-}
-
-/**
- * Opens the settings section, explaining the game if it is hidden.
- */
-
-function openSettingsFunction() {
-    let explainGameContainer = document.getElementById('explain-game-container');
-    if (explainGameContainer.classList.contains('d-none')) {
-        explainGameContainer.classList.remove('d-none');
-    }
-}
-
-/**
- * Closes the settings section explaining the game if it is visible.
- */
-
-function closeExplainGameContainer() {
-    let explainGameContainer = document.getElementById('explain-game-container');
-    if (!explainGameContainer.classList.contains('d-none')) {
-        explainGameContainer.classList.add('d-none');
+function openOverlay(overlayId){
+    let overlay = document.getElementById(`${overlayId}`);
+    if(overlay.classList.contains('d-none')){
+        overlay.classList.remove('d-none');
     }
 }
 
