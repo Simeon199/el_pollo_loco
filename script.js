0f5ba71d25c1e69760ecc97693755c0193ca40d5
Simeon199 <sim.ki@outlook.de>
2025-06-24 10:21:20 +0200

    improvements (only for development purposes) added

--- script.js
+++ script.js
@@ -59,19 +59,50 @@ let touchCSS = [
 ]; 
 
 document.addEventListener('DOMContentLoaded', async () => {
+    reloadSiteIfDeviceTypeSwitches();
     if(isLocationIndexPage()){
         if(isTouchDevice()){
-            console.log('I am in the if-statement for touch devices!');
             isTouch = true;
             loadTouchDeviceCSS();
             await loadTouchDeviceHTML();
         } else {
-            console.log('I am in the if-statement for desktop devices!');
             loadDesktopDeviceCSS();
             await loadDesktopDeviceHTML();
         }
     }
 });
+
+/* The following code serves only development purposes. It checks if a user is switching between desktop and touch device mode within the browser developer tools. */
+
+function reloadSiteIfDeviceTypeSwitches(){
+    let mq_one = window.matchMedia('(pointer: coarse)');
+    let mq_two = window.matchMedia('(hover: none)');
+    function checkAndReload(){
+        if(isSwitchingBetweenDeviceTypesIsHappening(mq_one, mq_two)){
+            window.location.reload();
+        }
+    }
+    mq_one.addEventListener('change', checkAndReload);
+    mq_two.addEventListener('change', checkAndReload);
+}
+
+/* Checks whether a user is switching from desktop type touch touch type or vice versa inside the browser dev tools*/
+
+function isSwitchingBetweenDeviceTypesIsHappening(mq_one, mq_two){
+    return isPointerCoarseAndHoverNoneAndNotTouchDevice(mq_one, mq_two) || isNeitherPointerCoarseNorHoverNoneAndTouchDevice(mq_one, mq_two);
+}
+
+/* Checks if the user has switched to a touch-like environment but the app is still in desktop mode */
+
+function isPointerCoarseAndHoverNoneAndNotTouchDevice(mq_one, mq_two){
+    return mq_one.matches && mq_two.matches && !isTouch;
+}
+
+/* Checks if the user has switched to desktop-like environment but the app is still in touch mode */
+
+function isNeitherPointerCoarseNorHoverNoneAndTouchDevice(mq_one, mq_two){
+    return (!mq_one || !mq_two.matches) && isTouch;
+}
 
 function loadTouchDeviceCSS(){
     touchCSS.forEach(link => {
