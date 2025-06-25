4889d06bb26cfb9d4a1bf6823fba923fa87f7b3b
Simeon199 <sim.ki@outlook.de>
2025-06-25 12:36:17 +0200

    some minor bugs solved for touch-device

--- js/touch-device.js
+++ js/touch-device.js
@@ -27,6 +27,63 @@ function setGlobalVariablesInTouchEvent(){
     });
 }
 
+function settingGlobalVariablesInKeyDownOrTouchStartEvent(event) {
+    world.character.isSoundIconInteraction = isEventOfTypeTouchAndSoundIconTriggered(event);
+    if (!world.character.isSoundIconInteraction) {
+        setKeyPressedVariablesRight(event);
+        someKeyWasPressedAgain = new Date().getTime();
+        world.character.wasRandomKeyOncePressed = wasRandomKeyOncePressed;
+        world.character.someKeyWasPressedAgain = someKeyWasPressedAgain;
+        world.character.isKeyPressed = isKeyPressed;
+    }
+}
+
+/**
+ * Determines if the event is a touch event and whether the sound icon was triggered. Prevents further key handling if the event targets a sound icon.
+ *
+ * @param {Event} event - The touch event to evaluate.
+ * @returns {boolean} - Returns `true` if the event is a touch event and the sound icon was triggered; otherwise, `false`.
+ */
+
+function isEventOfTypeTouchAndSoundIconTriggered(event) {
+    return event.type.startsWith('touch') && checkIfSoundIconWasTriggered(event) == true;
+}
+
+/**
+ * Checks if the event target matches the sound-on or sound-off icons.
+ *
+ * @param {Event} event - The event object to check.
+ * @returns {boolean} - Returns `true` if the event target is either the sound-on or sound-off icon; otherwise, `false`.
+ */
+
+function checkIfSoundIconWasTriggered(event) {
+    let soundOnIcon = document.getElementById('sound-on-icon');
+    let soundOffIcon = document.getElementById('sound-off-icon');
+    if (event.target == soundOffIcon) {
+        return true;
+    } else if (event.target == soundOnIcon) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Sets key press variables based on the event type, ensuring proper handling for touch events. 
+ * Skips setting variables if the event is a touch event and the sound icon is triggered.
+ *
+ * @param {Event} event - The event object that may be a key press or touch event.
+ */
+
+function setKeyPressedVariablesRight(event) {
+    if (isEventOfTypeTouchAndSoundIconTriggered(event)) {
+        return;
+    } else {
+        wasRandomKeyOncePressed = true;
+        isKeyPressed = true;
+    }
+}
+
 /**
  * Sets global variables when a key or touch event ends.
  */
