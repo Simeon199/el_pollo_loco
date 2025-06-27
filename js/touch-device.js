1dfadfd0f9bc220225f34bd0727f13f32cf349db
Simeon199 <sim.ki@outlook.de>
2025-06-27 10:25:17 +0200

    simplified functions in touch-device.js

--- js/touch-device.js
+++ js/touch-device.js
@@ -7,8 +7,6 @@ let momentKeySpaceWasReleased = 0;
 function addAllRemainingEventListenersWhenInitGame(isTouch) {
     if(isTouch && isGamePlaying){
         setGlobalVariablesInTouchEvent(); 
-        // touchStartHandler();
-        // touchEndHandler();
         settingStyleForTouchDevice();
     }
 }
@@ -149,27 +147,6 @@ function settingGlobalVariablesInKeyUpOrTouchEndEvent() {
     world.character.isKeyPressed = isKeyPressed;
 }
 
-// function touchStartHandler() { 
-//     if (isGamePlaying) {
-//         handleButtonLeftTouchStart();
-//         handleButtonRightTouchStart();
-//         handleJumpTouchStart();
-//         handleSpacebarTouchStart();
-//         handleThrowTouchStart();
-//     }
-// }
-
-// function handleButtonLeftTouchStart(){
-//     let buttonLeftTouch = document.getElementById('buttonLeft');
-//     if(buttonLeftTouch){
-//         buttonLeftTouch.addEventListener('touchstart', (event) => {
-//             event.preventDefault();
-//             prepareForThrowingLeft();
-//             buttonLeftTouch.style.background = 'rgb(75, 61, 35)';
-//         }, {passive: false});
-//     }
-// }
-
 /**
  * Prepares the player to throw an object to the left by setting the appropriate keyboard properties.
  * Sets `keyboard.LEFT` to true and ensures that `rightForThrow` is false while `leftForThrow` is true.
@@ -183,17 +160,6 @@ function prepareForThrowingLeft() {
     keyboard.leftForThrow = true;
 }
 
-// function handleButtonRightTouchStart(){
-//     let buttonRightTouch = document.getElementById('buttonRight');
-//     if(buttonRightTouch){
-//         buttonRightTouch.addEventListener('touchstart', (event) => {
-//             event.preventDefault();
-//             prepareForThrowingRight();
-//             buttonRightTouch.style.background = 'rgb(75, 61, 35)';
-//         }, {passive: false});
-//     }
-// }
-
 /**
  * Prepares the player to throw an object to the right by setting the appropriate keyboard properties.
  * Sets `keyboard.RIGHT` to true and ensures that `leftForThrow` is false while `rightForThrow` is true.
@@ -206,111 +172,6 @@ function prepareForThrowingRight() {
     }
     keyboard.rightForThrow = true;
 }
-
-// function handleSpacebarTouchStart(){
-//     let spacebarTouch = document.getElementById('spacebar');
-//     let buttonUpTouch = document.getElementById('buttonUp');
-//     if(spacebarTouch){
-//         spacebarTouch.addEventListener('touchstart', (event) => {
-//             event.preventDefault();
-//             keyboard.SPACE = true;
-//             buttonUpTouch.style.background = 'rgb(75, 61, 35)';
-//         }, {passive: false});
-//     }
-// }
-
-// function handleJumpTouchStart(){
-//     let buttonUpTouch = document.getElementById('buttonUp');
-//     if(buttonUpTouch){
-//         buttonUpTouch.addEventListener('touchstart', (event) => {
-//             event.preventDefault();
-//             keyboard.SPACE = true;
-//             buttonUpTouch.style.background = 'rgb(75, 61, 35)';
-//         }, {passive: false});
-//     }
-// }
-
-// function handleThrowTouchStart(){
-//     let buttonThrowTouch = document.getElementById('buttonThrow');
-//     if(buttonThrowTouch){
-//         buttonThrowTouch.addEventListener('touchstart', (event) => {
-//             event.preventDefault();
-//             keyboard.keyD = true;
-//             buttonThrowTouch.style.background = 'rgb(75, 61, 35)';
-//         }, {passive: false});
-//     }
-// }
-
-// function touchEndHandler() {
-//     if (isGamePlaying) { 
-//         handleButtonLeftTouchEnd();
-//         handleButtonRightTouchEnd();
-//         handleJumpTouchEnd();
-//         handleSpacebarTouchEnd();
-//         handleThrowTouchEnd();
-//     }
-// }
-
-// function handleButtonLeftTouchEnd(){
-//     let buttonLeftTouch = document.getElementById('buttonLeft');
-//     if(buttonLeftTouch){
-//         buttonLeftTouch.addEventListener('touchend', (event) => {
-//             event.preventDefault();
-//             keyboard.LEFT = false;
-//             world.audioManager.muteSound(true, 'walking_sound');
-//             buttonLeftTouch.style.background = 'wheat';
-//         }, {passive: false});
-//     }
-// }
-
-// function handleButtonRightTouchEnd(){
-//     let buttonRightTouch = document.getElementById('buttonRight');
-//     if(buttonRightTouch){
-//         buttonRightTouch.addEventListener('touchend', (event) => {
-//             event.preventDefault();
-//             keyboard.RIGHT = false;
-//             world.audioManager.muteSound(true, 'walking_sound');
-//             buttonRightTouch.style.background = 'wheat';
-//         }, {passive: false});
-//     }
-// }
-
-// function handleJumpTouchEnd(){
-//     let buttonUpTouch = document.getElementById('buttonUp');
-//     buttonUpTouch.addEventListener('touchend', (event) => {
-//         event.preventDefault();
-//         keyboard.SPACE = false;
-//         world.character.isKeySpaceReleased = true;
-//         momentKeySpaceWasReleased = new Date().getTime();
-//         buttonUpTouch.style.background = 'wheat';
-//     }, {passive: false});
-// }
-
-// function handleSpacebarTouchEnd(){
-//     let spacebarTouch = document.getElementById('spacebar');
-//     let buttonUpTouch = document.getElementById('buttonUp');
-//     if(spacebarTouch){
-//         spacebarTouch.addEventListener('touchend', (event) => {
-//             event.preventDefault();
-//             keyboard.SPACE = false;
-//             world.character.isKeySpaceReleased = true;
-//             momentKeySpaceWasReleased = new Date().getTime();
-//             buttonUpTouch.style.background = 'wheat';
-//         }, {passive: false});
-//     }
-// }
-
-// function handleThrowTouchEnd(){
-//     let buttonThrowTouch = document.getElementById('buttonThrow');
-//     if(buttonThrowTouch){
-//         buttonThrowTouch.addEventListener('touchend', (event) => {
-//             event.preventDefault();
-//             world.utilityClass.checkThrowObjects();
-//             keyboard.keyD = false;
-//             buttonThrowTouch.style.background = 'wheat';
-//         }, {passive: false});
-//     }
-// }
 
 /**
  * Displays the canvas and hides the intro image when the screen orientation is correct.
