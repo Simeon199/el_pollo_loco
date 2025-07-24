517b18119d5d4774ed5eef9b750c400232ffaeb2
Simeon199 <sim.ki@outlook.de>
2025-07-24 22:06:54 +0200

    changes added in script.js and eventHandlers.js

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,56 +1,148 @@
+let mqTouch = window.matchMedia('(hover: none)');
+let mqDesktop = window.matchMedia('(hover: hover)');
+let mqMediumDesktop = window.matchMedia('(min-width: 1025px) and (orientation: landscape)');
+let mqSmallDesktop = window.matchMedia('(max-width: 1024px) and (orientation: landscape)');
+let privacyOrImprintTouchActivated = false;
+
 let clickEventsHandleOnIndexPage = [
     {
         condition: (target) => isSettingsButtonPressed(target),
-        handler: async () => await loadComponent(explainGamePopUp, 'explain-game-container')  
+        handler: async (event, target) => await loadComponent(explainGamePopUp, 'explain-game-container')  
     },
     {
         condition: (target) => isAllIconsButtonPressed(target),
-        handler: async () =>  await loadComponent(showAllIconsPopUp, 'all-icons-container-overlay')
+        handler: async (event, target) =>  await loadComponent(showAllIconsPopUp, 'all-icons-container-overlay')
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#imprint'),
-        handler: () => redirectToWebPage('../imprint/imprint.html')
+        handler: (event, target) => redirectToWebPage('../imprint/imprint.html')
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#privacy'),
-        handler: () => redirectToWebPage('../privacy_policy/privacy_policy.html')
+        handler: (event, target) => redirectToWebPage('../privacy_policy/privacy_policy.html')
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#playIcon'),
-        handler: () => startGameAndSetStyleForDesktopDevice()
+        handler: (event, target) => startGameAndSetStyleForDesktopDevice()
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#exit-game-container'),
-        handler: () => setStyleForExitGameContainerAndResetGame()
+        handler: (event, target) => setStyleForExitGameContainerAndResetGame()
     },
     {
         condition: (target) => isSoundIconClicked(target),
-        handler: () => turnSoundOnOrOff()
+        handler: (event, target) => turnSoundOnOrOff()
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up'),
-        handler: async () => await loadComponent(privacyImprintOverlay, 'privacy-imprint-overlay')
+        handler: async (event, target) => await loadComponent(privacyImprintOverlay, 'privacy-imprint-overlay')
     }
 ]
 
 let clickEventsHandleOnImprintPage = [
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#main-page-link'),
-        handler: () => redirectToWebPage('../index.html')
+        handler: (event, target) => redirectToWebPage('../index.html')
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#privacy'),
-        handler: () => redirectToWebPage('../privacy_policy/privacy_policy.html')
+        handler: (event, target) => redirectToWebPage('../privacy_policy/privacy_policy.html')
     }
 ]
 
 let clickEventsHandleOnPrivacyPage = [
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#main-page-link'),
-        handler: () => redirectToWebPage('../index.html')
+        handler: (event, target) => redirectToWebPage('../index.html')
     },
     {
         condition: (target) => isContainerTouchedOrClicked(target, '#imprint'),
-        handler: () => redirectToWebPage('../imprint/imprint.html')
+        handler: (event, target) => redirectToWebPage('../imprint/imprint.html')
+    }
+]
+
+let touchStartEvents = [
+    {
+        condition: (target) => areTouchControlButtonsTouched(target),
+        handler: (event, target) => preventDefaultAndHandleAllSwitchCasesForTouchStart(event, target)
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#exit-game-container'),
+        handler: (event, target) => setExitGameContainersButtonStyle()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#main-page-link'),
+        handler: (event, target) => changeMainPageLinkColorOnTouchStart()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up'),
+        handler: (event, target) => setDivBackgroundColor('privacy-and-imprint-pop-up', 'gold')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#all-icons-button'),
+        handler: (event, target)  => setDivBackgroundColor('all-icons-button', 'gold')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#play-again-after-losing'),
+        handler: (event, target) => changeBackgroundOfPlayAgainButtonPressed('play-again-after-losing', 'goldenrod')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#play-again-after-winning'),
+        handler: (event, target) => changeBackgroundOfPlayAgainButtonPressed('play-again-after-winning', 'goldenrod')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#imprint-touch'),
+        handler: (event, target) => setDivBackgroundColor('imprint-touch', 'goldenrod')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#privacy-touch'),
+        handler: (event, target) => setDivBackgroundColor('privacy-touch', 'goldenrod')
+    }
+]
+
+let touchEndEvents = [
+    {
+        condition: (target) => areTouchControlButtonsTouched(target),
+        handler: (event, target) => preventDefaultAndHandleAllSwitchCasesForTouchEnd(event, target)
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#exit-game-container'),
+        handler: (event, target) => setStyleForExitGameContainerAndResetGame()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#playIcon'),
+        handler: (event, target) =>  startGameAndSetStyleForTouchDevice()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#main-page-link'),
+        handler: (event, target) => redirectToWebPage('../index.html')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up'),
+        handler: (event, target) => setDivBackgroundAndShowPrivacyAndImprint()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#settings-container-touch'),
+        handler: (event, target) => setDivBackgroundAndShowSettingsPopUp()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#all-icons-button'),
+        handler: (event, target)  => setDivBackgroundAndShowAllIconSources()
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#play-again-after-losing'),
+        handler: (event, target) => changeBackgroundOfPlayAgainButtonPressed('play-again-after-losing', 'gold')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#play-again-after-winning'),
+        handler: (event, target) => changeBackgroundOfPlayAgainButtonPressed('play-again-after-winning', 'gold')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#imprint-touch'),
+        handler: (event, target) => setDivBackgroundColor('imprint-touch', 'gold')
+    },
+    {
+        condition: (target) => isContainerTouchedOrClicked(target, '#privacy-touch'),
+        handler: (event, target) => setDivBackgroundColor('privacy-touch', 'gold')
     }
 ]
--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-let mqTouch = window.matchMedia('(hover: none)');
-let mqDesktop = window.matchMedia('(hover: hover)');
-let mqMediumDesktop = window.matchMedia('(min-width: 1025px) and (orientation: landscape)');
-let mqSmallDesktop = window.matchMedia('(max-width: 1024px) and (orientation: landscape)');
-let privacyOrImprintTouchActivated = false;
-
 document.addEventListener('DOMContentLoaded', () => {
     checkDeviceForMobileOrDesktopType();
     handleAllClickEvents();
-    handleAllTouchStartEvents();
-    handleAllTouchEndEvents();
+    handleAllTouchEventsIfUserOnIndexPage();
     handleAllChangeEvents();
     handleOrientationChange();
     handleKeyUpEvents();
@@ -22,30 +15,38 @@ function handleOrientationChange(){
 }
 
 function handleAllChangeEvents(){
-    mqTouch.addEventListener('change', () => {
-        setTimeout(() => {
-            reloadOnDeviceTypeSwitch();
-        }, 100);
-    });
-    mqDesktop.addEventListener('change', () => {
-        setTimeout(() => {
-            reloadOnDeviceTypeSwitch();
-        }, 100);
-    });
+    mqTouch.addEventListener('change', reloadDeviceAfterTimeOut);
+    mqDesktop.addEventListener('change', reloadDeviceAfterTimeOut);
+
     mqMediumDesktop.addEventListener('change', (event) => {
-        if(event.matches){
-            if(isLinksImagesTouchVisible()){
-                hideLinksImagesTouchVisible();
-            }
-        }
+        hideLinksImagesTouchIfEventMatches(event);
     });
+
     mqSmallDesktop.addEventListener('change', (event) => {
-        if(event.matches){
-            if(isLinksImagesTouchHidden()){
-                showLinksImagesTouchVisible();
-            }
-        }
+        showLinksImagesTouchIfEventMatches(event);
     });
+}
+
+function showLinksImagesTouchIfEventMatches(event){
+    if(event.matches){
+        if(isLinksImagesTouchHidden()){
+            showLinksImagesTouchVisible();
+        }
+    }
+}
+
+function hideLinksImagesTouchIfEventMatches(event){
+    if(event.matches){
+        if(isLinksImagesTouchVisible()){
+            hideLinksImagesTouchVisible();
+        }
+    }
+}
+
+function reloadDeviceAfterTimeOut(){
+    setTimeout(() => {
+        reloadOnDeviceTypeSwitch();
+    }, 100);
 }
 
 function setOverlayToFullViewport(){
@@ -78,16 +79,22 @@ function isHeightToWidthRatioBelowThreshold(){
 
 function handleAllClickEvents(){
     if(isLocationWebPage('/index.html')){
-        handleAccordingClickEvents(clickEventsHandleOnIndexPage); 
+        handleAccordingEvents(clickEventsHandleOnIndexPage, 'click'); 
     } else if(isLocationWebPage('/privacy_policy/privacy_policy.html')){
-        handleAccordingClickEvents(clickEventsHandleOnPrivacyPage);
+        handleAccordingEvents(clickEventsHandleOnPrivacyPage, 'click');
     } else if(isLocationWebPage('/imprint/imprint.html')){
-        handleAccordingClickEvents(clickEventsHandleOnImprintPage);
+        handleAccordingEvents(clickEventsHandleOnImprintPage, 'click');
     }
 }
 
-async function handleAccordingClickEvents(eventHandler){
-    document.addEventListener('click', (event) => {
+function handleAllTouchEventsIfUserOnIndexPage(){
+    handleAccordingEvents(touchStartEvents, 'touchstart');
+    handleAccordingEvents(touchEndEvents, 'touchend');
+}
+
+async function handleAccordingEvents(eventHandler, eventType){
+    document.addEventListener(`${eventType}`, (event) => {
+        returnGlobalSettingsManagerDependingOnEventType(eventType, event);
         let target = event.target;
         for(let {condition, handler} of eventHandler){
             if(condition(target)){
@@ -98,43 +105,15 @@ async function handleAccordingClickEvents(eventHandler){
     }, {passive: false});
 }
 
-
-// Ursprüngliche Versionen 
-
-// function handleAllClickEvents(){
-//     document.addEventListener('click', async (event) => {
-//         let target = event.target;
-//         if(isLocationWebPage('/index.html')){
-//             handleClickEventsOnIndexPage(target); // event
-//         } else if(isLocationWebPage('/privacy_policy/privacy_policy.html')){
-//             handleClickEventsOnLinksOnPrivacyPolicyPage(target);
-//         } else if(isLocationWebPage('/imprint/imprint.html')){
-//             handleClickEventsOnLinksOnImprintPage(target);
-//         } 
-//     });
-// }
-
-// async function handleClickEventsOnIndexPage(event){
-//     if(isSettingsButtonPressed(target)){
-//         await loadComponent(explainGamePopUp, 'explain-game-container');
-//     } else if(isAllIconsButtonPressed(target)){ 
-//         await loadComponent(showAllIconsPopUp, 'all-icons-container-overlay');
-//     } else if(isContainerTouchedOrClicked(target, '#imprint')){
-//         redirectToWebPage('../imprint/imprint.html');
-//     } else if(isContainerTouchedOrClicked(target, '#privacy')){
-//         redirectToWebPage('../privacy_policy/privacy_policy.html');
-//     } else if(isContainerTouchedOrClicked(target, '#playIcon')){
-//         startGameAndSetStyleForDesktopDevice();
-//     } else if(isContainerTouchedOrClicked(target, '#exit-game-container')){
-//         setStyleForExitGameContainerAndResetGame();
-//     } else if(isContainerTouchedOrClicked(target, '#main-page-link')){
-//         redirectToWebPage('../index.html');
-//     } else if(isSoundIconClicked(target)){
-//         turnSoundOnOrOff();
-//     } else if(isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up')){
-//         await loadComponent(privacyImprintOverlay, 'privacy-imprint-overlay');
-//     } 
-// }
+function returnGlobalSettingsManagerDependingOnEventType(eventType, event){
+    if(eventType === 'touchstart'){
+        settingGlobalVariablesInKeyDownOrTouchStartEvent(event);
+    } else if(eventType === 'touchend'){
+        settingGlobalVariablesInKeyUpOrTouchEndEvent(event);
+    } else if(eventType === 'click') {
+        return ;
+    }
+}
 
 function isAllIconsButtonPressed(target){
     return isContainerTouchedOrClicked(target, '#all-icons-button') || isOneOfDesktopButtonContainersClicked(target);
@@ -144,65 +123,77 @@ function isSettingsButtonPressed(target){
     return isContainerTouchedOrClicked(target, '#settings-container') || isContainerTouchedOrClicked(target, '#settings-container-touch');
 }
 
-function handleAllTouchStartEvents(){
-    document.addEventListener('touchstart', (event) => {
-        let target = event.target;
-        settingGlobalVariablesInKeyDownOrTouchStartEvent(event);
-        if(areTouchControlButtonsTouched(target)){
-            preventDefaultAndHandleAllSwitchCasesForTouchStart(event, target);
-        } else if(isContainerTouchedOrClicked(target, '#exit-game-container')){
-            setExitGameContainersButtonStyle();
-        } else if(isContainerTouchedOrClicked(target, '#main-page-link')){
-            changeMainPageLinkColorOnTouchStart();
-        } else if(isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up')){
-            setDivBackgroundColor('privacy-and-imprint-pop-up', 'gold');
-        } else if(isContainerTouchedOrClicked(target, '#all-icons-button')){
-            setDivBackgroundColor('all-icons-button', 'gold');
-        } else if(isContainerTouchedOrClicked(target, '#settings-container-touch')){
-            setDivBackgroundColor('settings-container-touch', 'gold');
-        } else if(isContainerTouchedOrClicked(target, '#play-again-after-losing')){
-            changeBackgroundOfPlayAgainButtonPressed('play-again-after-losing', 'goldenrod');
-        } else if(isContainerTouchedOrClicked(target, '#play-again-after-winning')){
-            changeBackgroundOfPlayAgainButtonPressed('play-again-after-winning', 'goldenrod');
-        } else if(isContainerTouchedOrClicked(target, '#imprint-touch')){
-            setDivBackgroundColor('imprint-touch', 'goldenrod');
-        } else if(isContainerTouchedOrClicked(target, '#privacy-touch')){
-            setDivBackgroundColor('privacy-touch', 'goldenrod');
-        }
-    }, {passive: false});
+// function handleAllTouchStartEvents(){
+//     document.addEventListener('touchstart', (event) => {
+//         let target = event.target;
+//         settingGlobalVariablesInKeyDownOrTouchStartEvent(event);
+//         if(areTouchControlButtonsTouched(target)){
+//             preventDefaultAndHandleAllSwitchCasesForTouchStart(event, target);
+//         } else if(isContainerTouchedOrClicked(target, '#exit-game-container')){
+//             setExitGameContainersButtonStyle();
+//         } else if(isContainerTouchedOrClicked(target, '#main-page-link')){
+//             changeMainPageLinkColorOnTouchStart();
+//         } else if(isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up')){
+//             setDivBackgroundColor('privacy-and-imprint-pop-up', 'gold');
+//         } else if(isContainerTouchedOrClicked(target, '#all-icons-button')){
+//             setDivBackgroundColor('all-icons-button', 'gold');
+//         } else if(isContainerTouchedOrClicked(target, '#settings-container-touch')){
+//             setDivBackgroundColor('settings-container-touch', 'gold');
+//         } else if(isContainerTouchedOrClicked(target, '#play-again-after-losing')){
+//             changeBackgroundOfPlayAgainButtonPressed('play-again-after-losing', 'goldenrod');
+//         } else if(isContainerTouchedOrClicked(target, '#play-again-after-winning')){
+//             changeBackgroundOfPlayAgainButtonPressed('play-again-after-winning', 'goldenrod');
+//         } else if(isContainerTouchedOrClicked(target, '#imprint-touch')){
+//             setDivBackgroundColor('imprint-touch', 'goldenrod');
+//         } else if(isContainerTouchedOrClicked(target, '#privacy-touch')){
+//             setDivBackgroundColor('privacy-touch', 'goldenrod');
+//         }
+//     }, {passive: false});
+// }
+
+// function handleAllTouchEndEvents(){
+//     document.addEventListener('touchend', (event) => {
+//         let target = event.target
+//         settingGlobalVariablesInKeyUpOrTouchEndEvent(event);
+//         if(areTouchControlButtonsTouched(target)){
+//            preventDefaultAndHandleAllSwitchCasesForTouchEnd(event, target);
+//         } else if(isContainerTouchedOrClicked(target, '#exit-game-container')){
+//             setStyleForExitGameContainerAndResetGame();
+//         } else if(isContainerTouchedOrClicked(target, '#playIcon')){
+//             startGameAndSetStyleForTouchDevice();
+//         } else if(isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up')){
+//             setDivBackgroundAndShowPrivacyAndImprint();
+//         } else if(isContainerTouchedOrClicked(target, '#all-icons-button')){
+//             setDivBackgroundAndShowAllIconSources();
+//         } else if(isContainerTouchedOrClicked(target, '#settings-container-touch')){
+//             setDivBackgroundAndShowSettingsPopUp();
+//         } else if(isContainerTouchedOrClicked(target, '#main-page-link')){
+//             redirectToWebPage('../index.html');
+//         } else if(isContainerTouchedOrClicked(target, '#play-again-after-losing')){
+//             changeBackgroundOfPlayAgainButtonPressed('play-again-after-losing', 'gold');
+//         } else if(isContainerTouchedOrClicked(target, '#play-again-after-winning')){
+//             changeBackgroundOfPlayAgainButtonPressed('play-again-after-winning', 'gold');
+//         } else if(isContainerTouchedOrClicked(target, '#imprint-touch')){
+//             setDivBackgroundColor('imprint-touch', 'gold');
+//         } else if(isContainerTouchedOrClicked(target, '#privacy-touch')){
+//             setDivBackgroundColor('privacy-touch', 'gold');
+//         }
+//     }, {passive: false});
+// }
+
+async function setDivBackgroundAndShowPrivacyAndImprint(){
+    setDivBackgroundColor('privacy-and-imprint-pop-up', 'goldenrod');
+    await loadComponent(privacyImprintOverlay, 'privacy-imprint-overlay'); 
 }
 
-function handleAllTouchEndEvents(){
-    document.addEventListener('touchend', (event) => {
-        let target = event.target
-        settingGlobalVariablesInKeyUpOrTouchEndEvent(event);
-        if(areTouchControlButtonsTouched(target)){
-           preventDefaultAndHandleAllSwitchCasesForTouchEnd(event, target);
-        } else if(isContainerTouchedOrClicked(target, '#exit-game-container')){
-            setStyleForExitGameContainerAndResetGame();
-        } else if(isContainerTouchedOrClicked(target, '#playIcon')){
-            startGameAndSetStyleForTouchDevice();
-        } else if(isContainerTouchedOrClicked(target, '#privacy-and-imprint-pop-up')){
-            setDivBackgroundColor('privacy-and-imprint-pop-up', 'goldenrod');
-            showPrivacyAndImprintOverlay();
-        } else if(isContainerTouchedOrClicked(target, '#all-icons-button')){
-            setDivBackgroundColor('all-icons-button', 'goldenrod');
-            showAllIconSourcesPopUp();
-        } else if(isContainerTouchedOrClicked(target, '#settings-container-touch')){
-            setDivBackgroundColor('settings-container-touch', 'goldenrod');
-            showExplainGamePopUp();
-        } else if(isContainerTouchedOrClicked(target, '#main-page-link')){
-            redirectToWebPage('../index.html');
-        } else if(isContainerTouchedOrClicked(target, '#play-again-after-losing')){
-            changeBackgroundOfPlayAgainButtonPressed('play-again-after-losing', 'gold');
-        } else if(isContainerTouchedOrClicked(target, '#play-again-after-winning')){
-            changeBackgroundOfPlayAgainButtonPressed('play-again-after-winning', 'gold');
-        } else if(isContainerTouchedOrClicked(target, '#imprint-touch')){
-            setDivBackgroundColor('imprint-touch', 'gold');
-        } else if(isContainerTouchedOrClicked(target, '#privacy-touch')){
-            setDivBackgroundColor('privacy-touch', 'gold');
-        }
-    }, {passive: false});
+async function setDivBackgroundAndShowAllIconSources(){
+    setDivBackgroundColor('all-icons-button', 'goldenrod');
+    await loadComponent(showAllIconsPopUp, 'all-icons-container-overlay');
+}
+
+async function setDivBackgroundAndShowSettingsPopUp(){
+    setDivBackgroundColor('settings-container-touch', 'goldenrod');
+    await loadComponent(explainGamePopUp, 'explain-game-container'); 
 }
 
 function handleClickEventsOnLinksOnPrivacyPolicyPage(target){
